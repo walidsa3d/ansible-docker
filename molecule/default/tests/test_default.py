e90d36c92fb0b52657596261669f1ef3b51e6d7a
walid <walid.sa3d@gmail.com>
2024-07-13 15:42:44 +0100

    first commit

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,8 @@
+def test_docker_is_installed(host):
+    docker = host.package("docker")
+    assert docker.is_installed
+
+def test_docker_running_and_enabled(host):
+    docker = host.service("docker")
+    assert docker.is_running
+    assert docker.is_enabled
