4bf72b70313d3d042bc7c9505979627619663781
walid <walid.sa3d@gmail.com>
2024-07-26 10:44:53 +0100

    test: use molecule, vagrant and ansible verifier

--- molecule/default/tests/test_default.py
+++
@@ -1,8 +0,0 @@
-def test_docker_is_installed(host):
-    docker = host.package("docker-ce")
-    assert docker.is_installed
-
-# def test_docker_running_and_enabled(host):
-#     docker = host.service("docker")
-#     assert docker.is_running
-#     #assert docker.is_enabled
